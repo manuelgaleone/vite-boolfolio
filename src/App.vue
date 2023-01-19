ce61ec3abcc255d7e29dd92ccec276ce3060fd62
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-19 15:31:25 +0100

    Fixed api errors with const

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,55 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-    components: {
-        HelloWorld,
-    }
+    data() {
+        return {
+            api_url : 'http://127.0.0.1:8000',
+            projects: {},
+            error: "",
+        }
+    },
+    methods : {
+        getProjects(url) {
+            axios.get(url)
+            .then(response => {
+                console.log(response.data);
+                this.projects = response.data;
+            })
+            .catch(error => {
+                console.error(error)
+                this.error = error.message
+            })
+        }
+    },
+    mounted() {
+    this.getProjects(this.api_url + '/api/projects');
+  }
 }
+
 </script>
 
 <template>
-    <section class="vue-home">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
-                    <div class="logos">
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-                        </a>
+    <div class="main_content_wrapper">
+        <div class="main_content_elements">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4" v-for="project in projects.data">
+                        <div class="card-body text-center">
+                            <h4>
+                                {{ project.title }}
+                            </h4>
+                            <p>
+                                {{ project.body }}
+                            </p>
+                        </div>
                     </div>
-                    <HelloWorld />
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 </template>
 
 <style lang="scss">
-@use './styles/general.scss';
 
-.vue-home {
-    color: #2c3e50;
-    background: #181818;
-    transition: color 0.5s, background-color 0.5s;
-    line-height: 1.6;
-    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-        Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 15px;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
