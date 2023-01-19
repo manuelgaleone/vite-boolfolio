0c767c7ea069c17c2d3b20520ec04e89ace126f4
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-19 16:30:56 +0100

    Add jumbotron

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,23 @@ export default {
     <HeaderContent></HeaderContent>
     <div class="main_content_wrapper">
         <div class="main_content_elements">
-            <div class="container">
-                <div class="row m-0 justify-content-center p-4">
-                    <ProjectCard></ProjectCard>
+            <div class="jumbotron_wrapper">
+                <div class="jumbotron bg-light jumbotron_elements py-4">
+                    <div class="container">
+                        <h1 class="display-4">Ciao Mondo!</h1>
+                        <p class="lead">Questo è il mio Portfolio personale.</p>
+                        <hr class="my-4">
+                        <p>Utilizzo delle chiamate API al mio Backend per funzionare!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="project_cards_wrapper">
+                <div class="project_cards_elements">
+                    <div class="container">
+                        <div class="row m-0 justify-content-center p-4">
+                            <ProjectCard></ProjectCard>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
