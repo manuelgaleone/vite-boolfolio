17581dfa5b535b05a35b3e0fbfbcd3e278f6f4a6
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-24 16:48:49 +0100

    Fixed single view

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,53 +1,67 @@
 <script>
 export default {
-    name : 'Header Component'
-}
+  name: "Header Component",
+};
 </script>
 
 <template>
-<header class="main_header_wrapper">
+  <header class="main_header_wrapper">
     <div class="main_header_elements p-3">
-        <div class="container">
-            <div class="row justify-content-between align-items-center">
-                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 text-center">
-                    <img class="header_logo" src="../assets/imgs/identity_Tavola disegno 1 copia 3.png" alt="Brand Identity">
-                </div>
-                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 d-flex justify-content-center p-0 m-0 mt-3">
-                    <ul class="menu_list d-flex m-0 p-0">
-                        <li class="menu_element">
-                            <router-link class="menu_item text-white" :to="{ name: 'home'}">HOME</router-link>
-                        </li>
-                        <li class="menu_element">
-                            <router-link class="menu_item ml-15 text-white" :to="{ name: 'contacts'}">CONTACTS</router-link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+      <div class="container">
+        <div class="row justify-content-between align-items-center">
+          <div
+            class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 text-center"
+          >
+            <img
+              class="header_logo"
+              src="../assets/imgs/identity_Tavola disegno 1 copia 3.png"
+              alt="Brand Identity"
+            />
+          </div>
+          <div
+            class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 d-flex justify-content-center p-0 m-0 mt-3"
+          >
+            <ul class="menu_list d-flex m-0 p-0">
+              <li class="menu_element">
+                <router-link class="menu_item text-white" :to="{ name: 'home' }"
+                  >HOME</router-link
+                >
+              </li>
+              <li class="menu_element">
+                <router-link
+                  class="menu_item ml-15 text-white"
+                  :to="{ name: 'contacts' }"
+                  >CONTACTS</router-link
+                >
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
-</header>
+  </header>
 </template>
 
 <style lang="scss">
 .header_logo {
-    width:75px;
+  width: 75px;
 }
 
 .menu_element {
-    list-style-type: none;
+  list-style-type: none;
 }
 
 .menu_item {
-    color: black;
-    text-decoration: none;
-    font-weight: 400;
+  color: black;
+  text-decoration: none;
+  font-weight: 400;
 }
 
 .logo_header {
-    font-weight: 800;
+  font-weight: 800;
 }
 
 .main_header_wrapper {
-    background-color: transparent;
+  background-color: transparent;
 }
 </style>
--- src/views/SingleProjectView.vue
+++ src/views/SingleProjectView.vue
@@ -6,7 +6,7 @@ export default {
     data() {
         return {
             api_url: "http://127.0.0.1:8000",
-            projects: {},
+            project: '',
             error: "",
             loading: true
         }
@@ -28,7 +28,7 @@ export default {
         axios.get(link)
         .then(response => {
             if(response.data.success) {
-                this.projects = response.data;
+                this.project = response.data;
                 this.loading = false;
             } else {
                 
@@ -42,22 +42,22 @@ export default {
 
 <template>
 <div class="single_project_wrapper vh-100 d-flex justify-content-center flex-column">
-    <div class="single_project_elements py-4">
+    <div class="single_project_elements py-4" v-if="!loading">
         <div class="jumbotron_wrapper">
                 <div class="jumbotron_elements">
                     <div class="jumbotron_home">
                         <h1 class="jumbo_title m-0 text-center text-white">
-                            {{ projects.title }}
+                            {{ project.data.title }}
                         </h1>
                     </div>
                 </div>
             </div>
         <div class="container">
-            <div class="single_project text-center mt-5 mb-5" v-if="!loading">
-                <img class="project_image" :src="api_url + '/storage/' + projects.image" :alt="projects.data.title">
+            <div class="single_project text-center mt-5 mb-5">
+                <img class="project_image" :src="api_url + '/storage/' + project.data.image" :alt="project.data.title">
                 <div class="project_contents py-4 text-center">
-                    <p class="project_content">
-                        {{ projects.data.content }}
+                    <p class="project_content text-white">
+                        {{ project.data.content }}
                     </p>
                 </div>
             </div>
