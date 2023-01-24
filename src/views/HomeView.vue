586efda3ae085649b73432e6561dd60cb0656a58
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-24 10:52:33 +0100

    working on jumbo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,22 +12,28 @@ export default {
 <template>
 <div class="jumbotron_wrapper">
     <div class="jumbotron_elements">
+        <div class="home_jumbo">
+            <h1 class="minimal_title m-0 text-center text-white">
+                Ciao, sono <span class="fancy">Manuel</span>!
+            </h1>
+        </div>
+    </div>
+</div>
+<div class="img_text_wrapper">
+    <div class="img_text_elements mt-5 mb-5">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 p-0">
-                    <div class="jumbo_titles d-flex justify-content-center flex-column">
-                        <h1 class="jumbo_title m-0">
-                            Ciao, sono <span class="fancy">Manuel</span>!
-                        </h1>
-                        <p class="jumbo_subtitles mb-0 mt-3">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 p-0 mt-2">
+                    <div class="minimal_titles d-flex justify-content-center flex-column">
+                        <p class="minimal_subtitles mb-0 mt-3">
                             Mi diletto in tante, <strong>troppe cose</strong>.<br>
                             Comunemente sono un <strong>Full Stack Developer</strong>
                             e so fare anche il <strong>caffè</strong>!
                         </p>
                     </div>
                 </div>
-                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 p-0">
-                    <div class="jumbo_right p-5">
+                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 p-0 mt-2">
+                    <div class="minimal_right p-5">
 
                     </div>
                 </div>
@@ -35,7 +41,7 @@ export default {
         </div>
     </div>
 </div>
-<div class="home_wrapper vh-100">
+<div class="home_wrapper">
     <div class="projects_elements">
         <ProjectView></ProjectView>
     </div>
@@ -43,15 +49,35 @@ export default {
 </template>
 
 <style lang="scss">
-.jumbo_titles {
+.home_jumbo {
+	background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+	padding: 250px;
+}
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+
+.minimal_titles {
     padding: 100px;
 }
 
-.jumbo_title {
+.minimal_title {
     font-weight: 900;
+    font-size: 75px;
 }
 
-.jumbo_right {
+.minimal_right {
     background-image: url('../assets/imgs/pexels-photo-249798.jpeg');
     background-size: cover;
     background-repeat: no-repeat;
