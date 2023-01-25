e3d53f0a9b35512d9d36b5a00ad41e2d81c56c05
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-25 11:56:28 +0100

    fixed mail view

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,6 +1,47 @@
 <script>
+import axios from 'axios';
+
 export default {
-    name : 'Contacts View'
+    name : 'Contacts View',
+    data() {
+        return {
+            api_url: "http://127.0.0.1:8000",
+            name : '',
+            email: '',
+            content: '',
+            success: false,
+            loading: false
+        }
+    },
+    methods : {
+        sendForm() {
+            this.loading = true
+
+            console.log('name', this.name)
+            console.log('email', this.email)
+            console.log('content', this.content)
+
+            const data = {
+                'name' : this.name,
+                'email' : this.email,
+                'content' : this.content
+            }
+
+            axios.post(`${this.api_url}/api/leads`, data).then((response) =>{
+
+                this.success = response.data.success;
+
+                console.log(response);
+
+                if(this.success) {
+                    this.name = '',
+                    this.email = '',
+                    this.content = ''
+                }
+                this.loading = false;
+            })
+        }
+    }
 }
 </script>
 
@@ -21,22 +62,22 @@ export default {
                 <div class="contacts_elements mt-5 mb-5">
                     <!-- Wrapper container -->
                     <div class="container">
-                        <form id="contactForm">
+                        <form @submit.prevent="sendForm()" method="post">
                             <!-- Name input -->
                             <div class="mb-3">
-                                <input class="form-control" id="name" type="text" placeholder="Paolo? O meglio Giovanni?" />
+                                <input class="form-control" v-model="name" name="name" id="name" type="text" placeholder="Paolo? O meglio Giovanni?" />
                             </div>
                             <!-- Email address input -->
                             <div class="mb-3">
-                                <input class="form-control" id="emailAddress" type="email" placeholder="Nome@mail.com" />
+                                <input class="form-control" v-model="email" name="email" id="email" type="email" placeholder="Nome@mail.com" />
                             </div>
                             <!-- Message input -->
                             <div class="mb-3">
-                                <textarea class="form-control" id="message" type="text" placeholder="Raccontami qualcosa!" style="height: 10rem;"></textarea>
+                                <textarea class="form-control" v-model="content" name="content" id="content" type="text" placeholder="Raccontami qualcosa!" style="height: 10rem;"></textarea>
                             </div>
                             <!-- Form submit button -->
                             <div class="d-grid">
-                                <button class="btn btn-light btn-lg" type="submit"><span><i class="fa-solid fa-plane-departure"></i></span> Spedisci</button>
+                                <button class="btn btn-light btn-lg" :disabled="loading" type="submit" >{{loading ? 'sending' : 'Spedisci'}}</button>
                             </div>
                         </form>
                     </div>
